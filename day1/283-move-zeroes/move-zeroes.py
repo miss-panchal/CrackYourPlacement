13b430de392884128f5fb1c9289c0e3f3515e3cf
anshul <100788462+miss-panchal@users.noreply.github.com>
2024-07-26 23:31:20 +0530

    Time: 125 ms (62.14%) | Memory: 18.1 MB (51.07%) - LeetSync

---
+++ day1/283-move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        non_zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[non_zero] = nums[non_zero], nums[i]  # Swap non-zero element with the next non-zero position
+                non_zero += 1 
+        return nums
