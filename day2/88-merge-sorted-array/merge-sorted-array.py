6301a910600b6832cfa32456727d90d2509f885d
anshul <100788462+miss-panchal@users.noreply.github.com>
2024-07-27 20:15:26 +0530

    Time: 42 ms (41.82%) | Memory: 16.6 MB (12.09%) - LeetSync

---
+++ day2/88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        nums1[m:m+n] = nums2
+        
+        # Sort nums1 in-place
+        nums1.sort()
+        
