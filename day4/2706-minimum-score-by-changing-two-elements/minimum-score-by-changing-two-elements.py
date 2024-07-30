2c021e8252480ac863e20dfbfa61e6f53aaea3b8
anshul <100788462+miss-panchal@users.noreply.github.com>
2024-07-30 15:27:14 +0530

    Delete day4/2706-minimum-score-by-changing-two-elements directory

--- day4/2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def minimizeSum(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        return min(nums[n-1]-nums[2] , min(nums[n-2]-nums[1],nums[n-3]-nums[0]))
