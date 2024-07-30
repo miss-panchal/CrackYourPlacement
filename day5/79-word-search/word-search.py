7bf890c70c59bea9b6705d2f27199cb72c6b36a8
anshul <100788462+miss-panchal@users.noreply.github.com>
2024-07-30 22:53:20 +0530

    Time: 38 ms (99.29%) | Memory: 16.7 MB (20.24%) - LeetSync

--- day5/79-word-search/word-search.py
+++ day5/79-word-search/word-search.py
@@ -1,38 +1,40 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        n = len(board)
-        m = len(board[0])
-
-        for row in range(n):
-            for col in range(m):
-                if self.depthFirstSearch(row, col, word, board):
-                    return True
-        return False
-
-    def depthFirstSearch(
-        self, row: int, col: int, word: str, grid: List[List[str]]
-    ) -> bool:
-        if len(word) == 0:
-            return True
-        if (
-            row < 0
-            or row >= len(grid)
-            or col < 0
-            or col >= len(grid[0])
-            or grid[row][col] != word[0]
-        ):
+        m = len(board)
+        n = len(board[0])
+        if len(word) > m*n: 
             return False
 
-        result = False
-        grid[row][col] = "*"
+        counter = defaultdict(int)
+        for i in range(m):
+            for j in range(n):
+                counter[board[i][j]] += 1
 
-        offsets = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        for rowOffset, colOffset in offsets:
-            result = self.depthFirstSearch(
-                row + rowOffset, col + colOffset, word[1:], grid
-            )
-            if result:
-                break
+        if counter[word[0]] > counter[word[-1]]: 
+            word = word[::-1]
+        for char in word:
+            if counter[char] == 0: 
+                return False
+            counter[char] -= 1
 
-        grid[row][col] = word[0]
-        return result
+        def backtracking(i, j, k):
+            res = False
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and k < len(word) and board[i][j] == word[k]:
+                if k == len(word) - 1:
+                    return True
+
+                temp = board[i][j]
+                board[i][j] = ""
+                res = backtracking(i + 1, j, k + 1) or \
+                    backtracking(i - 1, j, k + 1) or \
+                    backtracking(i, j + 1, k + 1) or \
+                    backtracking(i, j - 1, k + 1)
+                board[i][j] = temp
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtracking(i, j, 0):
+                    return True
+
+        return False
